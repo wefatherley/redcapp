8781a019fbc5b809d1274324f41ccaf69dd38afd
Will Fatherley <wefatherley@gmail.com>
2021-01-30 18:30:11 -0500

    adds first pass for sql migration writer

--- core/__init__.py
+++ core/__init__.py
@@ -9,7 +9,7 @@ LOGGER = getLogger(__name__)
 
 class Project:
     """WIP"""
-    
+
     def __init__(self, host=None, path=None, token=None):
         """Initialize API connector and metadata for a project"""
         if host is None and path is None and token is None:
@@ -31,6 +31,12 @@ class Project:
                 return self.metadata.load_record(data)
 
     def import_resource(self, resource, data, **parameters):
+        """WIP"""
+        if not hasattr(self, "api"):
+            raise NotImplementedError("No API connector")
+        pass
+
+    def delete_resource(self, resource, data, **parameters):
         """WIP"""
         if not hasattr(self, "api"):
             raise NotImplementedError("No API connector")
--- core/metadata.py
+++ core/metadata.py
@@ -1,8 +1,8 @@
 """Metadata object"""
-from csv import DictWriter
+from csv import DictReader, DictWriter
 from datetime import date, datetime, time
 from decimal import Context, Decimal, ROUND_HALF_UP
-from json import dumps
+from itertools import groupby
 from logging import getLogger
 from re import compile, finditer, sub
 
@@ -10,117 +10,173 @@ from re import compile, finditer, sub
 LOGGER = getLogger(__name__)
 
 
-class Metadata(dict):
-    """Abstraction for REDCap metadata, and corresponding functionality"""
+class SQL:
+    create_schema = "CREATE SCHEMA IF NOT EXISTS {};\n"
+    create_table = "CREATE TABLE IF NOT EXISTS {}.{}();\n"
+    add_column = "ALTER TABLE {}.{} ADD COLUMN IF NOT EXISTS {} {};\n"
 
-    # metadata columns
-    columns = [
-        "field_name", "form_name", "section_header", "field_type", "field_label",
-        "select_choices_or_calculations", "field_note",
-        "text_validation_type_or_show_slider_number" ,"text_validation_min",
-        "text_validation_max", "identifier", "branching_logic", "required_field",
-        "custom_alignment", "question_number", "matrix_group_name", "matrix_ranking",
-        "field_annotation"
-    ]
 
-    # decimal context map
-    dcm = {
-        "number": Context(prec=None, rounding=ROUND_HALF_UP)
-        "number_1dp_comma_decimal": Context(prec=1, rounding=ROUND_HALF_UP)
-        "number_1dp": Context(prec=1, rounding=ROUND_HALF_UP)
-        "number_2dp_comma_decimal": Context(prec=2, rounding=ROUND_HALF_UP)
-        "number_2dp": Context(prec=2, rounding=ROUND_HALF_UP)
-        "number_3dp_comma_decimal": Context(prec=3, rounding=ROUND_HALF_UP)
-        "number_3dp": Context(prec=3, rounding=ROUND_HALF_UP)
-        "number_4dp_comma_decimal": Context(prec=4), rounding=ROUND_HALF_UP
-        "number_4dp": Context(prec=4, rounding=ROUND_HALF_UP)
-        "number_comma_decimal": Context(prec=None, rounding=ROUND_HALF_UP)
-    }
-    
-    load_cast_map = {
-        "date_dmy": lambda d: date.strptime(d, "%d-%m-%Y"),
-        "date_mdy": lambda d: date.strptime(d, "%m-%d-%Y"),
-        "date_ymd": lambda d: date.strptime(d, "%Y-%m-%d"),
-        "datetime_dmy": lambda d: datetime.strptime(d, "%d-%m-%Y %H:%M"),
-        "datetime_mdy": lambda d: datetime.strptime(d, "%m-%d-%Y %H:%M"),
-        "datetime_ymd": lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M"),
-        "datetime_seconds_dmy": lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S"),
-        "datetime_seconds_mdy": lambda d: datetime.strptime(d, "%m-%d-%Y %H:%M:%S"),
-        "datetime_seconds_ymd": lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S"),
-        "email": lambda s: s,
-        "integer": lambda s: s,
-        "alpha_only": lambda s: s,
-        "number": lambda n: Decimal(sub(r",", ".", n), context=DCM["number"]),
-        "number_1dp_comma_decimal": lambda n: Decimal(
+COLUMNS = [
+    "field_name", "form_name", "section_header", "field_type", "field_label",
+    "select_choices_or_calculations", "field_note",
+    "text_validation_type_or_show_slider_number" ,"text_validation_min",
+    "text_validation_max", "identifier", "branching_logic", "required_field",
+    "custom_alignment", "question_number", "matrix_group_name", "matrix_ranking",
+    "field_annotation"
+]
+
+
+DCM = { # decimal context map
+    "number": Context(prec=None, rounding=ROUND_HALF_UP)
+    "number_1dp_comma_decimal": Context(prec=1, rounding=ROUND_HALF_UP)
+    "number_1dp": Context(prec=1, rounding=ROUND_HALF_UP)
+    "number_2dp_comma_decimal": Context(prec=2, rounding=ROUND_HALF_UP)
+    "number_2dp": Context(prec=2, rounding=ROUND_HALF_UP)
+    "number_3dp_comma_decimal": Context(prec=3, rounding=ROUND_HALF_UP)
+    "number_3dp": Context(prec=3, rounding=ROUND_HALF_UP)
+    "number_4dp_comma_decimal": Context(prec=4), rounding=ROUND_HALF_UP
+    "number_4dp": Context(prec=4, rounding=ROUND_HALF_UP)
+    "number_comma_decimal": Context(prec=None, rounding=ROUND_HALF_UP)
+}
+
+
+RECORD_TYPE_MAP = {
+    "date_dmy": (
+        lambda d: date.strptime(d, "%d-%m-%Y"),
+        lambda d: d.strftime("%d-%m-%Y"),
+        "DATE",
+    ),
+    "date_mdy": (
+        lambda d: date.strptime(d, "%m-%d-%Y"),
+        lambda d: d.strftime("%m-%d-%Y"),
+        "DATE",
+    ),
+    "date_ymd": (
+        lambda d: date.strptime(d, "%Y-%m-%d"),
+        lambda d: d.strftime("%Y-%m-%d"),
+        "DATE",
+    ),
+    "datetime_dmy": (
+        lambda d: datetime.strptime(d, "%d-%m-%Y %H:%M"),
+        lambda d: d.strftime("%d-%m-%Y %H:%M"),
+        "DATETIME",
+    ),
+    "datetime_mdy": (
+        lambda d: datetime.strptime(d, "%m-%d-%Y %H:%M"),
+        lambda d: d.strftime("%m-%d-%Y %H:%M"),
+        "DATETIME",
+    ),
+    "datetime_ymd": (
+        lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M"),
+        lambda d: d.strftime("%Y-%m-%d %H:%M"),
+        "DATETIME",
+    ),
+    "datetime_seconds_dmy": (
+        lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S"),
+        lambda d: d.strftime("%Y-%m-%d %H:%M:%S"),
+        "DATETIME",
+    ),
+    "datetime_seconds_mdy": (
+        lambda d: datetime.strptime(d, "%m-%d-%Y %H:%M:%S"),
+        lambda d: d.strftime("%m-%d-%Y %H:%M:%S"),
+        "DATETIME",
+    ),
+    "datetime_seconds_ymd": (
+        lambda d: datetime.strptime(d, "%Y-%m-%d %H:%M:%S"),
+        lambda d: d.strftime("%Y-%m-%d %H:%M:%S"),
+        "DATETIME",
+    ),
+    "email": (lambda s: s, lambda s: s, "TEXT",),
+    "integer": (int, str, "INT",),
+    "alpha_only": (lambda s: s, lambda s: s, "TEXT",),
+    "number": (
+        lambda n: Decimal(sub(r",", ".", n), context=DCM["number"]),
+        lambda n: str(n),
+        "FLOAT"
+    ),
+    "number_1dp_comma_decimal": (
+        lambda n: Decimal(
             sub(r",", ".", n), context=DCM["number_1dp_comma_decimal"]
         ),
-        "number_1dp":  lambda n: Decimal(n, context=DCM["number_1dp"]),
-        "number_2dp_comma_decimal": lambda n: Decimal(
+        lambda n: sub(r"\.", ",", str(n)),
+        "FLOAT",
+    ),
+    "number_1dp": (
+        lambda n: Decimal(n, context=DCM["number_1dp"]),
+        lambda n: str(n),
+        "FLOAT",
+    ),
+    "number_2dp_comma_decimal": (
+        lambda n: Decimal(
             sub(r",", ".", n), context=DCM["number_2dp_comma_decimal"]
         ),
-        "number_2dp": lambda n: Decimal(n, context=DCM["number_2dp"]),
-        "number_3dp_comma_decimal": lambda n: Decimal(
+        lambda n: sub(r"\.", ",", str(n)),
+        "FLOAT",
+    ),
+    "number_2dp": (
+        lambda n: Decimal(n, context=DCM["number_2dp"]),
+        lambda n: str(n),
+        "FLOAT"
+    ),
+    "number_3dp_comma_decimal": (
+        lambda n: Decimal(
             sub(r",", ".", n), context=DCM["number_3dp_comma_decimal"]
         ),
-        "number_3dp": lambda n: Decimal(n, context=DCM["number_3dp"]),
-        "number_4dp_comma_decimal": lambda n: Decimal(
+        lambda n: sub(r"\.", ",", str(n)),
+        "FLOAT",
+    ),
+    "number_3dp": (
+        lambda n: Decimal(n, context=DCM["number_3dp"]),
+        lambda n: str(n),
+        "FLOAT",
+    ),
+    "number_4dp_comma_decimal": (
+        lambda n: Decimal(
             sub(r",", ".", n), context=DCM["number_4dp_comma_decimal"]
         ),
-        "number_4dp": lambda n: Decimal(n, context=DCM["number_4dp"]),
-        "number_comma_decimal": lambda n: Decimal(
+        lambda n: sub(r"\.", ",", str(n)),
+        "FLOAT",
+    ),
+    "number_4dp": (
+        lambda n: Decimal(n, context=DCM["number_4dp"]),
+        lambda n: str(n),,
+        "FLOAT",
+    ),
+    "number_comma_decimal": (
+        lambda n: Decimal(
             sub(r",", ".", n), context=DCM["number_comma_decimal"]
         ),
-        "phone_australia": lambda s: s,
-        "phone": lambda s: s,
-        "postalcode_australia": lambda s: s,
-        "postalcode_canada": lambda s: s,
-        "ssn": lambda s: s,
-        "time": lambda t: time.strptime(t, "%H:%M"),
-        "time_mm_ss": lambda t: time.strptime(t, "%M:%S"),
-        "vmrn": lambda s: s,
-        "Zipcode": lambda s: s,
-        "": lambda s: s,
-    }
+        lambda n: sub(r"\.", ",", str(n)),
+        "FLOAT"
+    ),
+    "phone_australia": (lambda s: s, lambda s: s, "TEXT",),
+    "phone": (lambda s: s, lambda s: s, "TEXT",),
+    "postalcode_australia": (lambda s: s, lambda s: s, "TEXT",),
+    "postalcode_canada": (lambda s: s, lambda s: s, "TEXT",),
+    "ssn": (lambda s: s, lambda s: s, "TEXT",),
+    "time": (
+        lambda t: time.strptime(t, "%H:%M"),
+        lambda t: t.strftime("%H:%M"),
+        "TIME",
+    ),
+    "time_mm_ss": (
+        lambda t: time.strptime(t, "%M:%S"),
+        lambda t: t.strftime("%M:%S"),
+        "TIME"
+    "vmrn": (lambda s: s, lambda s: s, "TEXT",),
+    "Zipcode": (lambda s: s, lambda s: s, "TEXT",),
+    "": (lambda s: s, lambda s: s, "TEXT",),
+}
 
-    dump_cast_map = {
-        "date_dmy": lambda d: d.strftime("%d-%m-%Y"),
-        "date_mdy": lambda d: d.strftime("%m-%d-%Y"),
-        "date_ymd": lambda d: d.strftime("%Y-%m-%d"),
-        "datetime_dmy": lambda d: d.strftime("%d-%m-%Y %H:%M"),
-        "datetime_mdy": lambda d: d.strftime("%m-%d-%Y %H:%M"),
-        "datetime_ymd": lambda d: d.strftime("%Y-%m-%d %H:%M"),
-        "datetime_seconds_dmy": lambda d: d.strftime("%Y-%m-%d %H:%M:%S"),
-        "datetime_seconds_mdy": lambda d: d.strftime("%m-%d-%Y %H:%M:%S"),
-        "datetime_seconds_ymd": lambda d: d.strftime("%Y-%m-%d %H:%M:%S"),
-        "email": lambda s: s,
-        "integer": int,
-        "alpha_only": lambda s: s,
-        "number": lambda n: str(n),
-        "number_1dp_comma_decimal": lambda n: sub(r"\.", ",", str(n)),
-        "number_1dp": lambda n: str(n),
-        "number_2dp_comma_decimal": lambda n: sub(r"\.", ",", str(n)),
-        "number_2dp": lambda n: str(n),
-        "number_3dp_comma_decimal": lambda n: sub(r"\.", ",", str(n)),
-        "number_3dp": lambda n: str(n),
-        "number_4dp_comma_decimal": lambda n: sub(r"\.", ",", str(n)),
-        "number_4dp": lambda n: str(n),
-        "number_comma_decimal": lambda n: sub(r"\.", ",", str(n)),
-        "phone_australia": lambda s: s,
-        "phone": lambda s: s,
-        "postalcode_australia": lambda s: s,
-        "postalcode_canada": lambda s: s,
-        "ssn": lambda s: s,
-        "time": lambda t: t.strftime("%H:%M"),
-        "time_mm_ss": lambda t: t.strftime("%M:%S"),
-        "vmrn": lambda s: s,
-        "Zipcode": lambda s: s,
-        "": lambda s: s,
-    }
 
-    load_variable_re = compile(r"\[[\w()]+\]")
-    load_operator_re = compile(r"<>|\s=\s|\w=\w")
-    dump_variable_re = compile(r"record\['[\w]+'\]")
-    dump_operator_re = compile(r"==|!=")
+LOAD_VARIABLE_RE = compile(r"\[[\w()]+\]")
+LOAD_OPERATOR_RE = compile(r"<>|\s=\s|\w=\w")
+DUMP_VARIABLE_RE = compile(r"record\['[\w]+'\]")
+DUMP_OPERATOR_RE = compile(r"==|!=")
+
+
+class Metadata(dict):
+    """Abstraction for REDCap metadata, and corresponding functionality"""
 
     def __init__(self, raw_metadata=None, raw_field_names=None):
         """Contructor"""
@@ -152,7 +208,7 @@ class Metadata(dict):
     def load_logic(self, logic):
         """Convert REDCap logic syntax to Python logic syntax"""
         if not logic: return ""
-        for match in self.load_variable_re.finditer(logic):
+        for match in LOAD_VARIABLE_RE.finditer(logic):
             var_str = match.group(0).strip("[]")
             if "(" in var_str and ")" in var_str:
                 var_str = "___".join(
@@ -160,7 +216,7 @@ class Metadata(dict):
                 )
             var_str = "record['" + var_str + "']"
             logic = logic[:match.start()] + var_str + logic[:match.end()]
-        for match in self.load_operator_re.finditer(logic):
+        for match in LOAD_OPERATOR_RE.finditer(logic):
             ope_str = match.group(0)
             if ope_str == "=": ope_str = "=="
             elif ope_str == "<>": ope_str = "!="
@@ -171,13 +227,13 @@ class Metadata(dict):
         """Convert Python logic syntax to REDCap logic syntax"""
         if not logic:
             return ""
-        for match in self.dump_variable_re.finditer(logic):
+        for match in DUMP_VARIABLE_RE.finditer(logic):
             var_str = match.group(0).lstrip("record['").rstrip("']")
             if "___" in var_str:
                 var_str = "(".join(var_str.split("___")) + ")"
             var_str = "[" + var_str + "]"
             logic = logic[:match.start()] + var_str + logic[:match.end()]
-        for match in self.dump_operator_re.finditer(logic):
+        for match in DUMP_OPERATOR_RE.finditer(logic):
             ope_str = match.group(0)
             if ope_str == "==": ope_str = "="
             elif ope_str == "!=": ope_str = "<>"
@@ -187,21 +243,25 @@ class Metadata(dict):
     def load_record(self, record):
         """Return record with Python typing"""
         for k,v in record:
-            v = self.load_cast_map[
+            v = RECORD_TYPE_MAP[
                 self[k]["text_validation_type_or_show_slider_number"]
-            ](v)
+            ][0](v)
         return record
 
     def dump_record(self, record):
         """Return Pythonic record as JSON-compliant dict"""
         for k,v in record:
-            v = self.dump_cast_map[
+            v = RECORD_TYPE_MAP[
                 self[k]["text_validation_type_or_show_slider_number"]
-            ](v)
+            ][1](v)
         return record
 
-    def write(self, path, fmt="csv"):
+    def write(self, path, fmt="csv", **kwargs):
         """Write formatted metadata to path"""
+        for metadatum in self:
+            self[metadatum]["branching_logic"] = self.dump_logic(
+                self[metadatum]["branching_logic"]
+            )
         if fmt == "csv":
             with open(path, "w", newline="") as fp:
                 writer = DictWriter(fp, fieldnames=self.columns)
@@ -209,12 +269,33 @@ class Metadata(dict):
                 for metadatum in self.raw_metadata:
                     writer.writerow(metadatum)
                 for metadatum in self.values():
-                    metadatum["branching_logic"] = self.dump_logic(
-                        metadatum["branching_logic"]
-                    )
                     writer.writerow(metadatum)
-        elif fmt == "sql_migration": pass
-        elif fmt == "html_table": pass
+        elif fmt == "sql_migration":
+            migration = open(path, "w")
+            if "schema" in kwargs:
+                migration.write(SQL.create_schema.format(schema))
+            else:
+                schema = "public"
+            if "sql_by" not in kwargs:
+                sql_by = "field_type"
+            key = lambda d: d[sql_by]
+            for table, columns in groupby(
+                sorted(list(self.values()) + self.raw_metadata, key=key),
+                key=key
+            ):
+                migration.write(SQL.create_table.format(pubic, table))
+                for column in columns:
+                    migration.write(SQL.add_column.format(
+                        column, RECORD_TYPE_MAP[
+                            column["text_validation_type_or_show_slider_number"]
+                        ][2]
+                    )
+        elif fmt == "html_table":
+            pass
+        for metadatum in self:
+            self[metadatum]["branching_logic"] = self.load_logic(
+                self[metadatum]["branching_logic"]
+            )
 
 
 __all__ = ["Metadata"]
